7a4d5ee97448cbedc9fb0d51903830a5ba2db90d
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2016-02-12 20:13:50 -0600

    Sync Correction

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,12 +65,12 @@
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
-<<<<<<< HEAD
+
                         
                         <li class="dropdown">
-=======
+
                            <li class="dropdown">
->>>>>>> origin/master
+
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
